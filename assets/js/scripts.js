e9be7ef0a6cc60729bbe06778f2c86c60d7ef75b
Mauricio <mauricio.aravenao@usach.cl>
2024-04-12 17:00:17 -0400

    se añaden funciones a la pagina y archivo js

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correectamente...")
+    })
+});
+
+$(document).ready(function(){
+    $(".oculta1").click(function(){
+      $(".info1").toggle();
+    });
+  });
+
+$(document).ready(function(){
+    $(".oculta2").click(function(){
+      $(".info2").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".oculta3").click(function(){
+      $(".info3").toggle();
+    });
+});
+
